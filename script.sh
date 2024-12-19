59c20924c1b19104b439fb95ca45673b6edf5c56
Cristiandis <112336919+Cristiandis@users.noreply.github.com>
2024-12-19 16:30:36 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-filename=$(curl -sL https://ipa.aspy.dev/discord/stable/ | grep ipa | tail -n 2 | cut -f 2 -d '>' | cut -f 1 -d '<')
+filename=$(curl -sL https://ipa.aspy.dev/discord/stable/Discord_253.0.ipa)
 node index.js "https://ipa.aspy.dev/discord/stable/${filename:1}"
